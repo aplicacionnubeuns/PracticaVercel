9b4f5fe76fc71d43b7010a00800856c279c194ef
aplicacionnubeuns <155849705+aplicacionnubeuns@users.noreply.github.com>
2024-11-02 16:58:59 -0500

    Update index.php

--- api/index.php
+++ api/index.php
@@ -24,7 +24,7 @@
       <header class="blog-header py-3">
         <div class="row flex-nowrap justify-content-between align-items-center">
           <div class="col-12 text-center">
-            <a class="blog-header-logo text-dark" href="#">Diario STIC</a>
+            <a class="blog-header-logo text-dark" href="#">DESPLIEGUE PAAS CON VERCEL Y GOOGLE CLOUD</a>
           </div>
         </div>
       </header>
